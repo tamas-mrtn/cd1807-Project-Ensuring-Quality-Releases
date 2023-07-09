962b2e61958b6ec3e24a0a922f11097302ccae06
tamas-mrtn <tamas.marton@rtl.nl>
2023-07-09 15:32:50 +0200

    fix vm config

--- terraform/input.tf
+++ terraform/input.tf
@@ -4,3 +4,8 @@ variable "location" {}
 variable "application_type" {}
 variable "resource_type" {}
 variable "resource_group" {}
+
+# Network
+variable virtual_network_name {}
+variable address_prefix_test {}
+variable address_space {}
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -4,6 +4,6 @@ resource_group = "project-3"
 application_type = "myApplication"
 
 # Network
-virtual_network_name = ""
+virtual_network_name = "VNET"
 address_space = ["10.5.0.0/16"]
 address_prefix_test = "10.5.1.0/24"
