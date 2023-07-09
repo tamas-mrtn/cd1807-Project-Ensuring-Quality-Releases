151625355daa63fc32e72107c794f44ab969cf3e
tamas-mrtn <tamas.marton@rtl.nl>
2023-07-09 15:05:12 +0200

    change pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,61 @@
+provider "azurerm" {
+  features {}
+}
+terraform {
+  backend "azurerm" {
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
+    access_key           = ""
+  }
+}
+module "resource_group" {
+  source               = "../../modules/resource_group"
+  resource_group       = "${var.resource_group}"
+  location             = "${var.location}"
+}
+module "network" {
+  source               = "../../modules/network"
+  address_space        = "${var.address_space}"
+  location             = "${var.location}"
+  virtual_network_name = "${var.virtual_network_name}"
+  application_type     = "${var.application_type}"
+  resource_type        = "NET"
+  resource_group       = "${module.resource_group.resource_group_name}"
+  address_prefix_test  = "${var.address_prefix_test}"
+}
+
+module "nsg-test" {
+  source           = "../../modules/networksecuritygroup"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "NSG"
+  resource_group   = "${module.resource_group.resource_group_name}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  address_prefix_test = "${var.address_prefix_test}"
+}
+module "appservice" {
+  source           = "../../modules/appservice"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "AppService"
+  resource_group   = "${module.resource_group.resource_group_name}"
+}
+module "publicip" {
+  source           = "../../modules/publicip"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "publicip"
+  resource_group   = "${module.resource_group.resource_group_name}"
+}
+module "virtual_machine" {
+  source               = "../../modules/vm"
+  location             = var.location
+  resource_group       = module.resource_group.resource_group_name
+  application_type     = var.application_type
+  resource_type        = "VM"
+
+  public_ip_address_id = module.public_ip.public_ip_address_id
+  public_subnet_id     = module.network.subnet_id_test
+  admin_username       = "adminuser"
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,9 @@
+# Resource Group/Location
+location = "West Europe"
+resource_group = "project-3"
+application_type = "myApplication"
+
+# Network
+virtual_network_name = ""
+address_space = ["10.5.0.0/16"]
+address_prefix_test = "10.5.1.0/24"
