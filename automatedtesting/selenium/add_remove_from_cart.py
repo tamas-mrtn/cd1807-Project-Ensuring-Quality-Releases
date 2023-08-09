6a3a72534daacc41656179cccd3eae757fed5399
tamas-mrtn <tamas.marton@rtl.nl>
2023-08-09 23:23:40 +0200

    modify selenium log

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+import sys
 import logging 
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
